060e9b394b5bd4b3d663e5744459b2ef254a69a1
StressedVibeCoder <stressedvibecoder@gmail.com>
2026-06-26 20:50:14 +0100

    Split estilo.css into base, forms, typography, components

--- includes/header.php
+++ includes/header.php
@@ -9,7 +9,10 @@ $page_title = $page_title ?? 'Mundial 2026';
     <meta charset="UTF-8">
     <title><?= htmlspecialchars($page_title) ?></title>
     <link href="https://fonts.googleapis.com/css2?family=Advent+Pro:wght@400;700&family=VT323&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="estilo.css">
+    <link rel="stylesheet" href="base.css">
+    <link rel="stylesheet" href="forms.css">
+    <link rel="stylesheet" href="typography.css">
+    <link rel="stylesheet" href="components.css">
 </head>
 <body>
     <section class="cyberpunk black both">
